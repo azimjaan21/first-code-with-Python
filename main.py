e33d04e46f70f17dd1ce7b8c9e174df1ad2b811f
AzimJaan Akhtamov <azimaxtamov2oo2@mail.ru>
2024-06-18 20:04:28 +0500

    #lesson_6 -> User Current Age <-

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print('sabr!')
-print('oz qoldi')
 # Azimjaan started new journey !
+
+birth_year = int(input("Please, fill the birth year:"))
+age = 2024 - birth_year
+print("You're " + str(age) + " years old")
